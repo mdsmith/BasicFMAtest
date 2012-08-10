159ef33f6d6df2143335996330b2de24c088671b
Martin Smith <martin.audacis@gmail.com>
2012-08-10 12:53:01 -0700

    Now with looped testing

--- fmaTest.cpp
+++ fmaTest.cpp
@@ -15,14 +15,17 @@ int main()
     double elapsedTime;
     gettimeofday(&t1, NULL);
 
+    for (int j = 0; j < 10000; j++)
+    {
     float sum = 0.0f;
     for (int i = 0; i < ARRAY_LEN; i++)
         sum =  arr[i] *0.2f + sum;
+    }
 
     gettimeofday(&t2, NULL);
     elapsedTime = (t2.tv_sec -t1.tv_sec) * 1000.0;
     elapsedTime += (t2.tv_usec - t1.tv_usec) / 1000.0;
-    cout << "Result: " << sum << " Time: " << elapsedTime << endl;
+    cout << "Result: " << sum << " Time per (mean): " << elapsedTime/10000 << endl;
 
     return 0;
 }
